76c782862fe7e097433eac26bf7bc067c037f918
Valerie Montoya <valeriemontoya90@gmail.com>
2015-06-30 08:50:45 +0200

    add child component ExpensesList

--- app/app.ts
+++ app/app.ts
@@ -1,9 +1,9 @@
 /// <reference path="../typings/tsd.d.ts" />
 import {Component, View, bootstrap, NgFor} from 'angular2/angular2';
 import {Expense, ExpensesServices} from 'services/expensesServices';
-import {ExpensesFilteredList} from 'services/expensesFilteredList';
 import {StorageServices} from 'services/storageServices';
 import {Category, Categories} from 'services/categories';
+import {ExpensesList} from 'components/expenses-list/expenses-list';
 
 @Component({
   selector: 'app',
@@ -12,41 +12,10 @@ import {Category, Categories} from 'services/categories';
 })
 @View({
   templateUrl: 'app.html',
-  directives: [NgFor]
+  directives: [NgFor, ExpensesList]
 })
 class App {
 
-  //Handle categories
-  categoriesServices: Categories;
-  categories:Array<Category> = [];
-  
-  //Handle expenses
-  storageServices: StorageServices;
-  expensesServices: ExpensesServices;
-
-
-  constructor(expensesServices: ExpensesServices, categoriesServices: Categories, storageServices: StorageServices) {
-    this.expensesServices = expensesServices;
-    this.storageServices = storageServices;
-    this.categoriesServices = categoriesServices;
-
-    var expenses = this.expensesServices.getExpenses();
-    this.expensesFilteredList = new ExpensesFilteredList( expenses );
-    this.categories = this.categoriesServices.getCategoriesSaved();
-  }
-
-  onCategoryClicked(category: Category) : Array<Expense> {
-    var expensesListToShow = this.expensesFilteredList.addCategories(category);
-    return expensesListToShow;
-  }
-
-  load() {
-    this.expensesServices.setExpenses(this.storageServices.loadJson('expenses'));
-  }
-
-  save() {
-    this.storageServices.saveJson('expenses', this.expensesFilteredList.expenses);
-  }
 }
 
 bootstrap(App);
---
+++ app/components/expenses-list/expenses-list.ts
@@ -0,0 +1,52 @@
+import {Component, View, bootstrap, NgFor} from 'angular2/angular2';
+import {Expense, ExpensesServices} from 'services/expensesServices';
+import {StorageServices} from 'services/storageServices';
+import {Category, Categories} from 'services/categories';
+
+
+import {ExpensesFilteredList} from 'components/expenses-list/expensesFilteredList';
+
+@Component({
+  selector: 'expenses-list',
+  appInjector: [ExpensesServices, Categories, StorageServices]
+  //injectables: [ExpensesServices, Categories, StorageServices]
+})
+@View({
+  templateUrl: 'components/expenses-list/expenses-list.html',
+  directives: [NgFor]
+})
+export class ExpensesList {
+
+  expensesFilteredList:Array<ExpensesFilteredList>;
+  //Handle categories
+  categoriesServices: Categories;
+  categories:Array<Category> = [];
+  
+  //Handle expenses
+  storageServices: StorageServices;
+  expensesServices: ExpensesServices;
+
+
+  constructor(expensesServices: ExpensesServices, categoriesServices: Categories, storageServices: StorageServices) {
+    this.expensesServices = expensesServices;
+    this.storageServices = storageServices;
+    this.categoriesServices = categoriesServices;
+
+    var expenses = this.expensesServices.getExpenses();
+    this.expensesFilteredList = new ExpensesFilteredList( expenses );
+    this.categories = this.categoriesServices.getCategoriesSaved();
+  }
+
+  onCategoryClicked(category: Category) : Array<Expense> {
+    var expensesListToShow = this.expensesFilteredList.addCategories(category);
+    return expensesListToShow;
+  }
+
+  load() {
+    this.expensesServices.setExpenses(this.storageServices.loadJson('expenses'));
+  }
+
+  save() {
+    this.storageServices.saveJson('expenses', this.expensesFilteredList.expenses);
+  }
+}
